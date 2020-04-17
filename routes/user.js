e28b1291dc0150d231009e8460abee9fd331bf45
varunks99 <varunshiri99@gmail.com>
2020-04-17 21:16:51 +0530

    User registration

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/sign-up', (req, res) => {
+  res.render('user-signup')
+})
+
+router.post('/sign-up', (req, res) => {
+  User.register(new User({ username: req.body.username, email: req.body.email, name: req.body.name, contact: req.body.contact }), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, function() {
+                res.redirect('/');
+            })
+        }
+   });
+})
+
+module.exports = router;
